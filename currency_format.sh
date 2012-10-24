171bdf123574bb2afa41ff3ae5279f1e741f65d1
Zenlan Ltd <mszpak@zenlan.com>
2012-10-24 14:52:35 +0100

    Enhanced currency_format.sh to take command line param

--- currency_format.sh
+++ currency_format.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
- 
-sed -r -i 's/,"?\s?Â£?([0-9]+),?([0-9]+)?,?([0-9]+)?(\.[0-9]{2})\s?"?/,\1\2\3\4/g' *.csv
+
+if [ -z "$1" ]
+then
+  echo "usage: currency_format dataset/[\*]filename.csv (e.g. dataset/\*2012.csv)"
+fi 
+for infile in $1
+do
+    if [ -d "$infile" ] 
+    then
+		echo "$infile (dir)"
+    else
+		echo $infile	  
+		sed -r -i 's/,"?\s?£?([0-9]+),?([0-9]+)?,?([0-9]+)?(\.[0-9]{2})\s?"?/,\1\2\3\4/g' "$infile"
+    fi
+done
+
